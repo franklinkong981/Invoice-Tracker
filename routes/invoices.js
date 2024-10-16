2fbafb0f0237ff48eb521f55f6f62c40303f626a
Franklin Kong <franklinkong981@gmail.com>
2024-10-16 12:46:52 -0700

    Added route that returns a specific invoice

--- routes/invoices.js
+++ routes/invoices.js
@@ -15,4 +15,36 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.get('/:id', async (req, res, next) => {
+  try {
+    const invoice_id = req.params.id;
+    const result = await db.query(
+      `SELECT i.id, i.comp_code, i.amt, i.paid, i.add_date, i.paid_date, c.name, c.description
+      FROM invoices AS i 
+      INNER JOIN companies AS c ON (i.comp_code = c.code) 
+      WHERE id = $1`, 
+      [invoice_id]);
+    if (result.rows.length === 0) {
+      throw new ExpressError(`The invoice with id of ${invoice_id} cannot be found in the database`, 404);
+    }
+
+    const data = result.rows[0];
+    const invoice = {
+      id: data.id,
+      amt: data.amt,
+      paid: data.paid,
+      add_date: data.add_date,
+      paid_date: data.paid_date,
+      company: {
+        code: data.comp_code,
+        name: data.name,
+        description: data.description
+      }
+    }
+    return res.status(200).json({"invoice": invoice});
+  } catch(e) {
+    return next(e);
+  }
+});
+
 module.exports = router;
