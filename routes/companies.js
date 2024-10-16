91af964b7ab79153563f0a17fa96846e88acc87d
Franklin Kong <franklinkong981@gmail.com>
2024-10-16 00:03:45 -0700

    Added POST route that adds a company and returns it

--- routes/companies.js
+++ routes/companies.js
@@ -27,4 +27,18 @@ router.get('/:code', async (req, res, next) => {
   }
 });
 
+router.post('/', async (req, res, next) => {
+  try {
+    if (!req.body.code) throw new ExpressError("You must provide a code attribute in the body specifying the code of the company to add", 400);
+    if (!req.body.name) throw new ExpressError("You must provide a name attribute in the body specifying the name of the company to add", 400);
+    if (!req.body.description) throw new ExpressError("You must provide a description attribute in the body containing a brief description of the company to add", 400);
+    
+    const {code, name, description} = req.body;
+    const results = await db.query(`INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING *`, [code, name, description]);
+    return res.status(201).json({company: results.rows[0]});
+  } catch(e) {
+    return next(e);
+  }
+});
+
 module.exports = router;
