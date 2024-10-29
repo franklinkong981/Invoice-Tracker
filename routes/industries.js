f936aaf66a4edc8df9dfddaff4c8ff329e2c7a42
Franklin Kong <franklinkong981@gmail.com>
2024-10-29 14:42:30 -0700

    Added POST route for adding a company industry association

--- routes/industries.js
+++ routes/industries.js
@@ -29,4 +29,17 @@ router.post('/', async (req, res, next) => {
   }
 });
 
+router.post('/associate', async (req, res, next) => {
+  try {
+    if (!req.body.company_code) throw new ExpressError("You must provide a company code in the body specifying the code of the company you want to associate", 400);
+    if (!req.body.industry_code) throw new ExpressError("You must provide an industry code attribute in the body specifying the code of the industry you want to associate", 400);
+    
+    const {company_code, industry_code} = req.body;
+    const results = await db.query(`INSERT INTO companies_industries (company_code, industry_code) VALUES ($1, $2) RETURNING *`, [company_code, industry_code]);
+    return res.status(201).json({association: results.rows[0]});
+  } catch(e) {
+    return next(e);
+  }
+});
+
 module.exports = router;
